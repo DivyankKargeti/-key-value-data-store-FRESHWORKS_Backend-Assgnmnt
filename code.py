f770934692a2062ab4020a6fed94f16d84ef2d05
DivyankKargeti <divyankkargeti99@gmail.com>
2020-12-28 12:15:51 +0530

    Modified

--- code.py
+++ code.py
@@ -3,14 +3,14 @@ import threading
 from threading import*
 import time
 
-d={} #'d' is the dictionary in which we store data
+d={} #It is the dictionary in which we will be storing data
 
 #for create operation 
-#Usage: "create(key_name,value,timeout_value)" timeout is optional you can continue by passing two arguments without timeout
+#Usage: "create(key_name,value,timeout_value)" timeout is optional - We can also pass just 2 args
 
 def create(key,value,timeout=0):
     if key in d:
-        print("error: this key already exists") #error message1
+        print("ERROR: This key is already present in datastore") #ERROR Msg1
     else:
         if(key.isalpha()):
             if len(d)<(1024*1020*1024) and value<=(16*1024*1024): #constraints for file size less than 1GB and Jasonobject value less than 16KB 
@@ -21,16 +21,16 @@ def create(key,value,timeout=0):
                 if len(key)<=32: #constraints for input key_name capped at 32chars
                     d[key]=l
             else:
-                print("error: Memory limit exceeded!! ")#error message2
+                print("ERROR: Memory limit has exceeded ")#ERROR Msg2
         else:
-            print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")#error message3
+            print("ERROR: Invalind type key - key_name must contain only alphabets and no special characters or numbers")#ERROR Msg3
 
 #for read operation
 #Usage: "read(key_name)"
             
 def read(key):
     if key not in d:
-        print("error: given key does not exist in database. Please enter a valid key") #error message4
+        print("error: given key does not exist in database. Please enter a valid key") #ERROR Msg4
     else:
         b=d[key]
         if b[1]!=0:
@@ -38,7 +38,7 @@ def read(key):
                 stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
                 return stri
             else:
-                print("error: time-to-live of",key,"has expired") #error message5
+                print("error: time-to-live of",key,"has expired") #ERROR Msg5
         else:
             stri=str(key)+":"+str(b[0])
             return stri
@@ -48,7 +48,7 @@ def read(key):
 
 def delete(key):
     if key not in d:
-        print("error: given key does not exist in database. Please enter a valid key") #error message4
+        print("error: given key does not exist in database. Please enter a valid key") #ERROR Msg4
     else:
         b=d[key]
         if b[1]!=0:
@@ -56,7 +56,7 @@ def delete(key):
                 del d[key]
                 print("key is successfully deleted")
             else:
-                print("error: time-to-live of",key,"has expired") #error message5
+                print("error: time-to-live of",key,"has expired") #ERROR Msg5
         else:
             del d[key]
             print("key is successfully deleted")
